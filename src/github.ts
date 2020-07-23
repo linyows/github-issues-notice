f5d485fe0a62462ab498c3a6f59f0090bb48ec68
linyows <linyows@gmail.com>
2020-07-23 16:49:27 +0900

    add github response types

--- src/github.ts
+++ src/github.ts
@@ -81,3 +81,130 @@ export class Github {
     return JSON.parse(res.getContentText())
   }
 }
+
+export type User = {
+  login: string
+  id: number
+  node_id: string
+  avatar_url: string
+  gravatar_id: string
+  url: string
+  html_url: string
+  followers_url: string
+  following_url: string
+  gists_url: string
+  starred_url: string
+  subscriptions_url: string
+  organizations_url: string
+  repos_url: string
+  events_url: string
+  received_events_url: string
+  type: string
+  site_admin: boolean
+}
+
+export type Label = {
+  id: number
+  node_id: string
+  url: string
+  name: string
+  color: string
+  default: boolean
+  description: string
+}
+
+export type Milestone = {
+  url: string
+  html_url: string
+  labels_url: string
+  id: number
+  node_id: string
+  number: number
+  title: string
+  description: string
+  creator: User
+  open_issues: number
+  closed_issues: number
+  state: string
+  created_at: string
+  updated_at: string
+  due_on: string
+  closed_at: null|string
+}
+
+export type Team = {
+  id: number
+  node_id: string
+  url: string
+  html_url: string
+  name: string
+  slug: string
+  description: string
+  privacy: string
+  permission: string
+  members_url: string
+  repositories_url: string
+  parent: null
+}
+
+export type Issue = {
+  url: string
+  repository_url: string
+  labels_url: string
+  comments_url: string
+  events_url: string
+  html_url: string
+  id: number
+  node_id: string
+  number: number
+  title: string
+  user: User
+  labels: Label[]
+  state: string
+  locked: boolean
+  assignee: null|User
+  assignees: User[]
+  milestone: null|Milestone
+  comments: number
+  created_at: string
+  updated_at: string
+  author_association: string
+  body: string
+}
+
+export type PullRequest = {
+  url: string
+  id: number
+  node_id: string
+  html_url: string
+  diff_url: string
+  patch_url: string
+  issue_url: string
+  number: number
+  state: string
+  locked: boolean
+  title: string
+  user: User
+  body: string
+  created_at: string
+  updated_at: string
+  closed_at: null|string
+  merged_at: null|string
+  merge_commit_sha: string
+  assignee: null|User
+  assignees: User[]
+  requested_reviewers: User
+  requested_teams: Team
+  labels: Label[]
+  milestone: null|Milestone
+  draft: boolean
+  commits_url: string
+  review_comments_url: string
+  review_comment_url: string
+  comments_url: string
+  statuses_url: string
+  head: any
+  base: any
+  _links: any
+  author_association: string
+}
